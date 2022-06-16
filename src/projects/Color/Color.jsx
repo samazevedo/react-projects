b56fefe148b4fd38cac2200f3d472c2a9d69a1d7
samzevedo <samuel.azevedo@live.com>
2022-06-16 15:50:01 -0300

    add palette color generator basic structure

--- src/projects/Color/Color.jsx
+++ src/projects/Color/Color.jsx
@@ -1,5 +1,39 @@
+import { useState } from 'react'
+import { ColorStyled } from './Color.styled'
+
 function Color() {
-    return <div>Color</div>
+    const [color, setColor] = useState('')
+    const [error, setError] = useState(false)
+    const [list, setList] = useState([])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('working')
+    }
+    return (
+        <ColorStyled>
+            <header>
+                <h1>Palette Color Generator</h1>
+            </header>
+            <section>
+                <form onSubmit={() => handleSubmit}>
+                    <label htmlFor='color-hex'>Enter a value:</label>
+                    <input
+                        type='text'
+                        name='color-hex'
+                        id='color-hex'
+                        value={color}
+                        onChange={(e) => setColor(e.target.value)}
+                        placeholder='#A5FF6D'
+                    />
+                    <button type='submit'>Submit</button>
+                </form>
+            </section>
+            <section>
+                <div className='colors'>colors go here</div>
+            </section>
+        </ColorStyled>
+    )
 }
 
 export default Color
---
+++ src/projects/Color/Color.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const ColorStyled = styled.main`
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 0.5fr 1.5fr;
+    align-items: center;
+    justify-items: center;
+`
