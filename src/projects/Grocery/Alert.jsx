07ab33f1d4ea59786639e755dc9b62d3e2d30294
samzevedo <samuel.azevedo@live.com>
2022-06-25 19:16:02 -0300

    grocery list: add alert funct

--- src/projects/Grocery/Alert.jsx
+++ src/projects/Grocery/Alert.jsx
@@ -1,6 +1,18 @@
 import { AlertStyled } from './Grocery.styled'
-function Alert() {
-    return <AlertStyled>Alert</AlertStyled>
+import { useEffect } from 'react'
+
+function Alert({ message, type, showAlert }) {
+    useEffect(() => {
+        setTimeout(() => {
+            showAlert(false)
+        }, 3000)
+    }, [showAlert])
+
+    return (
+        <AlertStyled>
+            <p className={`alert ${type}`}>{message}</p>
+        </AlertStyled>
+    )
 }
 
 export default Alert
